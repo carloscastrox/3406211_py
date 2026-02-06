1b05313d8111f4de7b591a4eb8986912fa3f3d79
Carlos Andres Castro Jaramillo <cacjx84@gmail.com>
2026-02-06 14:49:28 -0500

    Update algoritmos.py

--- algoritmos.py
+++ algoritmos.py
@@ -11,9 +11,10 @@ average = sum / 3
 
 # Salidas
 print("La suma total es:", sum)
-print("El promedio es:", average)
+print("El promedio es:", int(average))
 
-#2. Área de un rectángulo: Solicitar la base y la altura de un rectángulo. Calcular y mostrar el área correspondiente.
+"""
+2. Área de un rectángulo: Solicitar la base y la altura de un rectángulo. Calcular y mostrar el área correspondiente.
 3. Conversión de temperatura: Solicitar una temperatura en grados Celsius y convertirla a grados Fahrenheit.
 4. Salario semanal: Solicitar la cantidad de horas trabajadas en la semana y el valor por hora. Calcular y mostrar el salario semanal.
 5. Salario con horas extra: Solicitar horas trabajadas y valor por hora. Si el empleado trabajó más de 40 horas, las horas adicionales se pagan al 150%. Calcular el salario total.
@@ -23,3 +24,4 @@ print("El promedio es:", average)
 9. Venta con IVA: Solicitar el valor de una venta sin impuestos. Calcular el IVA (19%) y mostrar el valor del IVA y el total con impuesto incluido.
 10. Compra de varios productos: Solicitar la cantidad de productos comprados y el precio de cada uno. Calcular el total de la compra.
 11. Comisión por ventas: Solicitar el valor total de ventas realizadas por un vendedor. Calcular una comisión del 5% y mostrar el total a recibir.
+"""
